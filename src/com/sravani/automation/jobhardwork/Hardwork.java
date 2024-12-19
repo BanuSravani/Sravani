a47c285fae643b636559355cc637aeb53cfd8f1b
BanuSravani <upparpalliSravani@gmail.com>
2024-12-19 11:29:59 +0530

    added hardwork.java and modified job1.java

---
+++ src/com/sravani/automation/jobhardwork/Hardwork.java
@@ -0,0 +1,8 @@
+package com.sravani.automation.jobhardwork;
+
+public class Hardwork {
+ public void sucess() {
+System.out.println("Do hardwork you will get sucess");
+
+}
+}
--- src/com/sravani/automation/jobhardwork/Job1.java
+++ src/com/sravani/automation/jobhardwork/Job1.java
@@ -1,5 +1,9 @@
 package com.sravani.automation.jobhardwork;
 
 public class Job1 {
+ 
+	public void subject() {
+	System.out.println("subject first job next");
 
+	}
 }
